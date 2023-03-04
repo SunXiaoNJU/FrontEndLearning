0394ea1e7b1a0a80bef393030ac3f47736817395
201250011 <2375367814@qq.com>
2023-03-04 20:51:58 +0800

    update

--- Vue/02_vite/src/App.js
+++ Vue/02_vite/src/App.js
@@ -10,11 +10,12 @@ export default {
   },
   // 在组件中注册子组件，子组件之间是相互独立的，不会互相影响。
   components: {
-    "my-button": MyButton,
+    MyButton,
   },
 
   template: `<h1>{{message}}</h1>
-  <my-button></my-button>
-  <my-button></my-button>
+  <MyButton></MyButton>
+  <MyButton></MyButton>
+  <MyButton></MyButton>
   `,
 };
